eed3f6c3f1a725bae5260072dbb3503d39cb0301
dragoljubilictech@gmail.com <dragoljubilictech@gmail.com>
2021-11-24 16:26:27 +0100

    Displaying a custom map marker

--- script.js
+++ script.js
@@ -21,28 +21,31 @@ if (navigator.geolocation)
 
       const coords = [latitude, longitude];
 
-      const mymap = L.map('map').setView(coords, 13);
+      const myMap = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(mymap);
+      }).addTo(myMap);
 
-      //   var circle = L.circle(coords, {
-      //     color: 'red',
-      //     fillColor: '#f03',
-      //     fillOpacity: 0.5,
-      //     radius: 500,
-      //   }).addTo(mymap);
+      myMap.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        console.log(lat, lng);
 
-      //   var polygon = L.polygon([coords, [51.503, -0.06], [51.51, -0.047]]).addTo(
-      //     mymap
-      //   );
-
-      var marker = L.marker(coords).addTo(mymap);
-      marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
-      //   circle.bindPopup('I am a circle.');
-      //   polygon.bindPopup('I am a polygon.');
+        const marker = L.marker([lat, lng]).addTo(myMap);
+        marker
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
