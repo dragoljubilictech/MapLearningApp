3e4ad23535105d0cd7a1869c48dfde000bee70a0
dragoljubilictech@gmail.com <dragoljubilictech@gmail.com>
2021-11-25 14:15:43 +0100

    Move marker on click

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords; //  [lat, lng]
@@ -17,6 +18,10 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
   }
 }
 
@@ -64,12 +69,14 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
   #myMap;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
   constructor() {
     this._gePosition();
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _gePosition() {
@@ -89,7 +96,7 @@ class App {
 
     const coords = [latitude, longitude];
 
-    this.#myMap = L.map('map').setView(coords, 13);
+    this.#myMap = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
@@ -237,6 +244,27 @@ class App {
           </div>
       `;
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+
+    this.#myMap.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+    console.log(workout);
+
+    //using the public interface
+    workout.click();
   }
 }
 
